99728c327a03387415bbe2034925cbd57ea7b17a
Lucas <lucasmiranda.da.silva8@gmail.com>
2024-09-11 09:04:49 -0300

    Imersao Dev

---
+++ app.js
@@ -0,0 +1,25 @@
+function pesquisar() {
+
+  let section = document.getElementById("resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+  console.log(campoPesquisa)
+
+  let resultados = "";
+
+  for (let dado of dados) {
+    if(dado.titulo.includes(campoPesquisa)){
+         
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Mais informações</a>
+        </div>
+    `;
+    }
+  }
+  section.innerHTML = resultados;
+}
